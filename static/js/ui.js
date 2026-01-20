c994f69bd08ac4ab3e4d4e61cfc99fef8ba98a83
starry-shivam <starry@krsh.dev>
2026-01-20 21:27:10 +0530

    Update auto theme icon
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- static/js/ui.js
+++ static/js/ui.js
@@ -13,23 +13,19 @@
     }
 
     function setAutoIcon() {
-    if (!icon) return;
+       if (!icon) return;
 
-    icon.innerHTML = `
-        <circle cx="12" cy="12" r="5"></circle>
-        <path d="M12 1v2"></path>
-        <path d="M12 21v2"></path>
-        <path d="M4.22 4.22l1.42 1.42"></path>
-        <path d="M18.36 18.36l1.42 1.42"></path>
-        <path d="M1 12h2"></path>
-        <path d="M21 12h2"></path>
-        <path d="M4.22 19.78l1.42-1.42"></path>
-        <path d="M18.36 5.64l1.42-1.42"></path>
+       icon.innerHTML = `
+          <!-- Outer circle -->
+          <circle cx="12" cy="12" r="9" fill="none" stroke="currentColor" stroke-width="2"></circle>
 
-        <!-- Moon overlay -->
-        <path d="M21 12.79A4 4 0 0117.21 7
-                 4 4 0 0021 12.79z"></path>
-    `;
+          <!-- Left half filled -->
+          <path d="
+              M12 3
+              A9 9 0 0 0 12 21
+            Z
+          " fill="currentColor"></path>
+       `;
     }
 
     function setIcon(theme, mode) {
